909ec02d22ae254d62aaeff2c9b9468ee8e2c722
semin0723 <45254558+semin0723@users.noreply.github.com>
2024-06-11 10:19:01 +0900

    add : d2d 엔진을 라이브러리로 전환하고, 라이브러리를 활용한 신규게임 파일 생성

---
+++ Win32Main/DemoApp.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "../D2DEngine/D2DEngine.h"
+
+class DemoApp : public WinApp
+{
+public:
+	DemoApp() = default;
+	~DemoApp() = default;
+
+	virtual void Initialize(HINSTANCE _hInstance) override;
+
+	virtual void Update() override;
+	virtual void Render(ID2D1HwndRenderTarget* _target) override;
+
+	virtual void Finalize() override;
+
+private:
+};
+
