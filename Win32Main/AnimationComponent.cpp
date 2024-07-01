5c84928a18ad79f59d131873d2fcb735908c9308
semin0723 <45254558+semin0723@users.noreply.github.com>
2024-07-01 17:00:24 +0900

    Add : AnimationComponent
    선언부 정의 추가

--- Win32Main/AnimationComponent.cpp
+++ Win32Main/AnimationComponent.cpp
@@ -1,1 +1,15 @@
 #include "AnimationComponent.h"
+
+AnimationComponent::AnimationComponent()
+{
+}
+
+AnimationComponent::~AnimationComponent()
+{
+	_animations.clear();
+}
+
+void AnimationComponent::ChangeAnimation(std::string& stateName)
+{
+	_curAnimationState = stateName;
+}
